d87c618ead6f1a27d02790dcac81b984405a7fc0
Sudhansu <sudhansum.tripathy@yahoo.com>
2020-11-11 00:50:47 +0100

    fixed assets

--- website/website/urls.py
+++ website/website/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from music import views
 
@@ -24,4 +26,4 @@ urlpatterns = [
     path('',include('music.urls')),
     path('add',views.add,name = 'add'),
     path('admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
